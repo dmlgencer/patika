f6854da29ed59b7543a62f528453542218a43c6e
Damla Gencer <damla.45gencer@gmail.com>
2022-10-28 20:52:25 +0300

    damla

---
+++ src/döngüler/daireninAlanıVeÇevresi.java
@@ -0,0 +1,21 @@
+package döngüler;
+
+import java.util.Scanner;
+
+public class daireninAlanıVeÇevresi {
+    public static void main(String[] args) {
+        int r;
+        Scanner input = new Scanner(System .in);
+        System.out.println("Dairenin Yarı Çapını Giriniz");
+        r = input.nextInt();
+
+
+        double alan,cevre , 𝜋=3.14,a;
+        alan = 𝜋*r*r;
+        cevre=2*𝜋*r;
+
+
+        System.out.println("Dairenin Alanı " +alan);
+        System.out.println("Dairenin Çevresi " +cevre);
+    }
+}
