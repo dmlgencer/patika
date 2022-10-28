78bb7722ecd8fade8cd59ade0be1665b04590c41
Damla Gencer <damla.45gencer@gmail.com>
2022-10-28 21:09:23 +0300

    damla

---
+++ src/temel_kavramlar/hipotenüs.java
@@ -0,0 +1,21 @@
+package temel_kavramlar;
+
+import java.util.Scanner;
+
+public class hipotenüs {
+    public static void main(String[] args) {
+        //değişkenlerimizi oluşturalım
+        int a, b;
+        double c;
+
+        //kullanıcıdan verilerimizi alalım
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("1. Kenarı Giriniz : ");
+        a = girdi.nextInt();
+        System.out.print("2. Kenarı Giriniz : ");
+        b = girdi.nextInt();
+
+        c= Math.sqrt((a*a)+(b*b));
+        System.out.print("Hipotenüs : " + c + " cm ");
+    }
+}
