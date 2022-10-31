8d23d71ef07ea545ee35af188fd37d88ed13d2a2
Damla Gencer <damla.45gencer@gmail.com>
2022-10-31 19:30:06 +0300

    damla

---
+++ src/mayınTarlası/Runner.java
@@ -0,0 +1,9 @@
+package mayınTarlası;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        MineSwepper mineSwepper = new MineSwepper(4,4);
+        mineSwepper.run();
+    }
+}
